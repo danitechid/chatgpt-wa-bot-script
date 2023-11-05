41d00e30c0c3a40deb983ca383912a20368212f5
Dani Ramdani <contact@danitechno.com>
2023-11-05 20:43:35 +0700

    Add files via upload

--- config/settings.js
+++ config/settings.js
@@ -1,21 +1,17 @@
-/*
- * Information
- * Creator / Developer: Dani Ramdani (Dani Techno.) - FullStack Engineer
- * Contact creator / Developer: 0895 1254 5999 (WhatsApp), contact@danitechno.com (Email)
-*/
-
-/* Thanks to
- * Dani Techno. - FullStack Engineer (Creator / Developer)
- * @danitech/wa-web-api (Library "WhatsApp Web API" provider)
-*/
-
 module.exports = {
   pairing_mode: true,
   session_folder_name: 'session',
   browser: ["Chrome (Windows)", "latest"],
   public_mode: true,
+  bot: {
+    name: 'Bot Name'
+  },
+  owner: {
+    name: 'Owner Name',
+    number: '628xxx'
+  },
   api: {
     url: 'https://daniapi.biz.id',
-    key: 'sk-danitechno'
+    key: 'Your-API-Key'
   }
 };
